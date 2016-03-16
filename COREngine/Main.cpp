6c342a13f44f8e5bc2a4ebda9e67ab2b604f9259
Kasiah Butler <snipercorgii@gmail.com>
2016-03-15 17:18:39 -0700

    Added SFWDraw
    Updated to Add SFWDraw to COREngine

--- COREngine/Main.cpp
+++ COREngine/Main.cpp
@@ -1,5 +1,6 @@
 #include "Vector2.h"
 #include "Matrix3.h"
+#include "sfwdraw.h"
 #include <iostream>
 
 int main()
@@ -7,8 +8,5 @@ int main()
 	COR::Vec2 a = { 2, 2 };
 	COR::Mat3 b = { 2, 3, 4, 5, 6, 7, 8, 9, 1 };
 
-	std::cout << b << std::endl;
-
-
-	system("pause");
+	return 0;
 }
